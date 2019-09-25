071237f0da8fb72dc99f43c24fc4a533f746761a
Kelby Styler <kelbystyler@gmail.com>
2019-09-24 17:47:34 -0700

    Initialize soil tile
    * Connect signal for mouse/touch events
    * Add functions to utility for handling tile damage and destruction

--- global/utility.gd
+++ global/utility.gd
@@ -4,8 +4,7 @@ extends Node
 #this script holds common functions that can be called by the game at any time
 
 #game specific signals
-signal loopObject
-signal generateCollider
+
 
 #general signals
 
@@ -31,13 +30,9 @@ var resolution = Vector2(1280, 720)
 
 
 #player variables (for saving and for the player script to grab)
-var final_score = 0
 
 var playerVariableDict = {
-#	"health" : maxHealth,
-#	"boostFuel" : maxBoostFuel,
-#	"bombCount" : startBombCount,
-#	"shotTime" : shotTime,
+
 	}
 
 # stored variables
@@ -109,28 +104,10 @@ func _ready():
 #	pass
 	
 	
-#func spawnEnemies(enemies, row): #to spawn
-#	var y = 0
-#	var count = 0
-#	var startPosition = Vector2(128 + 64/2,128 + 32)
-#
-#	for enemy in enemies:
-#		if enemy == 1:
-#			var instanceEnemy = enemyDict["1"].instance()
-#
-#			get_node("/root/main/gameSceneCam/gameStage").add_child(instanceEnemy)
-#			instanceEnemy.set_position(Vector2(startPosition.x + 64 * count,startPosition.y + 64 * y))
-#
-#			count += 1
-#		else:
-#			count += 1
-#	pass
-	
-	
 func sceneSwitch(scene): #add a "self" variable so the current scene can free itself before spawning the new screen
 	var instanceScene = stageSceneDict[scene].instance()
 	
-	get_node("/root/main/container_stage").add_child(instanceScene)
+	get_node("/root/main/stage_container").add_child(instanceScene)
 	
 #	if scene == "stageHighScore":
 #		print(final_score)
@@ -158,24 +135,6 @@ func spawnObject(object, parent, position):
 	pass
 	
 
-#func loopObject(object, position, velocity, ignore):
-#	var direction = -velocity
-#	var ray2D = RayCast2D.new()
-#
-#	object.add_child(ray2D)
-#	ray2D.set_as_toplevel(true)
-#	ray2D.set_enabled(true)
-#	ray2D.add_exception(ignore)
-#	ray2D.set_global_position(position)
-#	ray2D.set_collide_with_areas(true)
-#	ray2D.set_collide_with_bodies(false)
-#	ray2D.set_cast_to(direction * 100)
-#	ray2D.force_raycast_update()
-#	if ray2D.is_colliding():
-#		object.set_global_position(ray2D.get_collision_point())
-##		ray2D.queue_free()
-#	pass
-
 func getGameState():
 	return __gameState;
 
@@ -194,63 +153,6 @@ func getGameState():
 #	collisionNode.set_polygon(poolArray)
 #	return;
 
-
-#func saveHighscores (highscoreInfo : Dictionary):
-#	var pos = highscoreInfo.pos;
-#	var initials = highscoreInfo.initials;
-#	var score = highscoreInfo.score;
-#
-#	var highscores = utility.getHighscores();
-#	var outHighscores = [];
-#	var count = 0;
-#	for i in range(0, highscores.size()):
-#		if (pos == i):
-#			outHighscores.append({'pos': pos, 'initials': initials, 'score': score});
-#			count += 1;
-#
-#		var currHighscoreInfo = highscores[i];
-#		currHighscoreInfo.pos = count;
-#		outHighscores.append(currHighscoreInfo);
-#		continue;
-#	
-#	# getting the current game state and updating its HIGHSCORES property.
-#	var gameState = getGameState();
-#	gameState.HIGHSCORES = highscores;
-#
-#	__saveGameState();
-#	return;
-
-#func getHighscores () -> Array:
-#	var highscoresArr = getGameState().HIGHSCORES;
-#
-#	# if not an array, or no highscores, let make sure there aren't any saved
-#	if (typeof(highscoresArr) != TYPE_ARRAY) or (highscoresArr.size() == 0):
-#		var fileApi = File.new();
-#
-#		# if there is no file, set as empty array
-#		if not fileApi.file_exists(FILE_NAME):
-#			highscoresArr = [];
-#		else:
-#			fileApi.open("user://savegame.save", File.READ);
-#
-#			# else, get the info from json inside file
-#			while not fileApi.eof_reached():
-#				var gameState = parse_json(fileApi.get_line());
-#				highscoresArr = gameState.HIGHSCORES;
-#
-#			# close file
-#			fileApi.close()
-#
-#	# double check it is proper format, if not, empty array is sent.
-#	if (typeof(highscoresArr) != TYPE_ARRAY):
-#		highscoresArr = [];
-#		print("highscore was not in proper format");
-#
-#	return highscoresArr;
-
-#func setHighscores (paramVal : Array):
-#	__gameState.HIGHSCORES = paramVal;
-#	return;
 	
 func change_music(audio):
 	var stream_music = get_tree().get_current_scene().get_node("sound_player_music")
@@ -271,4 +173,14 @@ func play_sound_effect(sfx, bus, free):
 func sfx_finished(bus,free):
 	if free:
 		bus.queue_free()
+	pass
+	
+	
+func tile_damage(tile_health):
+	tile_health = tile_health - 1
+	return tile_health
+	
+	
+func tile_destroy(tile):
+	tile.queue_free()
 	pass
---
+++ soil_tile/soil_tile.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# soil tile that the user interacts with when attempting to locate gems within the excavation limits
+
+var soil_tile_health : int = 1 #number of touches needed to destroy the tile
+var soil_tile_dim : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$StaticBody2D.connect("input_event", self, "_action_input")
+	
+	soil_tile_dim = $Sprite.get_texture().get_size()
+	pass # Replace with function body.
+
+func _action_input(node_viewport, event, shape_id):
+	if event.is_pressed() and event.button_index == BUTTON_LEFT:
+		soil_tile_health = utility.tile_damage(soil_tile_health)
+		
+		if soil_tile_health == 0:
+			utility.tile_destroy(self)
+	pass
