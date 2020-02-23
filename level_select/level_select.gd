179e9a4977eb72e3205c281396867fd5939df038
Kelby Styler <kelbystyler@gmail.com>
2020-02-23 06:54:46 -0800

    Level location rarity tabs #23
    Rarity tabs that show locations for different level areas

--- level_select/level_select.gd
+++ level_select/level_select.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$actors/gui/mc/background/active/gui_cont/button_close_stage.connect("button_up", self, "_button_close_stage")
+	$actors/gui/mc/active/gui_cont/button_close_stage.connect("button_up", self, "_button_close_stage")
 	pass # Replace with function body.
 
 
