17ac8eccce172c436932951ad79f458e1bf19899
Kelby <kelbystyler@gmail.com>
2020-12-09 07:09:07 -0800

    Gem storage updates
    *Updated gem containers so they automatically show depending on if user has unlocked that size or not
    *Updated container script so the export variable assigns the size to the container. Utility script then handles placing gem in the correct containers based on that size assignment.

---
+++ gem_container_hbox/gem_container_hbox.gd
@@ -0,0 +1,27 @@
+extends HBoxContainer
+
+#script controls when to show gem storage containers or not
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#check utility script and choose to show or hide container
+	#depending on if user has unlocked them or not
+	
+	var unlock_dict = utility.unlock_dict
+	
+	if unlock_dict["gem_storage_small"] == true:
+		$gem_storage_small.show()
+		
+	if unlock_dict["gem_storage_med"] == true:
+		$gem_storage_med.show()
+		
+	if unlock_dict["gem_storage_large"] == true:
+		$gem_storage_large.show()
+		
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- gem_storage_container/gem_storage_container.gd
+++ gem_storage_container/gem_storage_container.gd
@@ -1,12 +1,19 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var max_gem_count_small : int = 9 #total number of gems this contianer can hold
+export var container_size : String #size of the storage container
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	utility.gem_small_container = self
+	if container_size == "small":
+		utility.gem_small_container = self
+	
+	if container_size == "med":
+		utility.gem_med_container = self
+		
+	if container_size == "large":
+		utility.gem_large_container = self
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
